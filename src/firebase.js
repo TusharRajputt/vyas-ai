a52bab001bf276bfe2832cf143c7173f5173abdb
Tushar Dev Singh <139161899+TusharRajputt@users.noreply.github.com>
2025-02-06 15:14:44 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTJ5VT8wN16pg9ja2lMaAiDyMgP0ZGGjM",
-  authDomain: "vyas-auth.firebaseapp.com",
-  projectId: "vyas-auth",
-  storageBucket: "vyas-auth.appspot.com",
-  messagingSenderId: "815458381003",
-  appId: "1:815458381003:web:fb1efccaf7f4eee3743f4e",
-  databaseURL: "https://vyas-ai-8e2d4-default-rtdb.firebaseio.com/"
+  apiKey: "AIzaSyArri2x_YmhsRYC85fDU1pNWYxuw3JjJmo",
+  authDomain: "vyas-ai-8e2d4.firebaseapp.com",
+  databaseURL: "https://vyas-ai-8e2d4-default-rtdb.firebaseio.com",
+  projectId: "vyas-ai-8e2d4",
+  storageBucket: "vyas-ai-8e2d4.firebasestorage.app",
+  messagingSenderId: "528127924335",
+  appId: "1:528127924335:web:a9b3e96ea1434fa627d59a",
+  measurementId: "G-GFW5LBKHMB"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-const database = getDatabase(app);
-
-export { auth, provider, database };
+const analytics = getAnalytics(app);
